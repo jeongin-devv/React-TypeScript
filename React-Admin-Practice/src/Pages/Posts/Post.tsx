9dd9e1c4f3ee29391b9c2b482d60fb59ac0778c9
jeongin-devv <jeongin.devv@gmail.com>
2022-08-01 00:24:23 +0900

    add DataProvider & SimpleList

--- React-Admin-Practice/src/Pages/Posts/Post.tsx
+++ React-Admin-Practice/src/Pages/Posts/Post.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { useTheme, useMediaQuery } from '@mui/material';
 import {
@@ -8,29 +9,32 @@ import PostFilter from './PostFilter';
 export default () => {
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
     <List filters={PostFilter}>
       {isSmall ? (
         <SimpleList
-            primaryText={record => record.title}
-            secondaryText={record => (
-                <ReferenceField label="User" source="userId" reference="users">
-                    <TextField source="name" />
-                </ReferenceField>
-            )}
+          primaryText={(record) => record.title}
+          // eslint-disable-next-line no-unused-vars
+          secondaryText={(record) => (
+            <ReferenceField label="User" source="userId" reference="users">
+              <TextField source="name" />
+            </ReferenceField>
+          )}
         />
       ) : (
         <Datagrid rowClick="edit">
-        <TextField source="id" />
-        <ReferenceField source="userId" reference="users">
+          <TextField source="id" />
+          <ReferenceField source="userId" reference="users">
+            {/* <TextField source="id" /> */}
+            <TextField source="name" />
+          </ReferenceField>
           {/* <TextField source="id" /> */}
-          <TextField source="name" />
-        </ReferenceField>
-        {/* <TextField source="id" /> */}
-        <TextField source="title" />
-        {/* <TextField source="body" /> */}
-        <EditButton />
-      </Datagrid>)}
+          <TextField source="title" />
+          {/* <TextField source="body" /> */}
+          <EditButton />
+        </Datagrid>
+      )}
     </List>
   );
-}
+};
--- React-Admin-Practice/src/Styles/themed-components.ts
+++ React-Admin-Practice/src/Styles/themed-components.ts
@@ -1,13 +1,13 @@
 import * as styledComponents from 'styled-components';
 
-import { Theme } from './theme';
+// import { Theme } from './theme';
 
 const {
   default: styled,
   css,
   keyframes,
   ThemeProvider,
-} = styledComponents as styledComponents.ThemedStyledComponentsModule<Theme>;
+} = styledComponents;
 
 export {
   css,
