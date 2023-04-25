c11fd4b7d22cdbc81c5b0acb112b7e54f8031645
jeongin-devv <jeongin.devv@gmail.com>
2023-04-25 23:09:41 +0900

    update

--- Nextjs-Practice/nextjs-create-app/pages/clients/[id]/[clientprojectId].tsx
+++ Nextjs-Practice/nextjs-create-app/pages/clients/[id]/[clientprojectId].tsx
@@ -1,7 +1,12 @@
+// 중첩 동적 라우팅
+// 중괄호로 폴더를 만들고, 안에 동적 라우팅을 위한 페이지를 넣으면
+// 중첩된 동적 라우팅을 사용 할 수 있다.
+
 import { useRouter } from 'next/router';
 
 const SelectedClientProjectPage = () => {
 	const router = useRouter();
+	// query 에서 id, clientprojectId  확인 가능
 	console.log(router.query);
 
 	return (
