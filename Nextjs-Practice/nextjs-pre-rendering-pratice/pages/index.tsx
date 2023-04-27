014edf80da1dbd61e1db75552316313ea2066fae
jeongin-devv <jeongin.devv@gmail.com>
2023-04-27 23:13:18 +0900

    init

---
+++ Nextjs-Practice/nextjs-pre-rendering-pratice/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
+  )
+}
