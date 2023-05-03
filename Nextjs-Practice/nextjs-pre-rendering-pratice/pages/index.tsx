20f74438c62d29d520b5f5f841536263d50e1f9b
jeongin-devv <jeongin.devv@gmail.com>
2023-05-03 21:58:49 +0900

    server side rendering example

--- Nextjs-Practice/nextjs-pre-rendering-pratice/pages/index.tsx
+++ Nextjs-Practice/nextjs-pre-rendering-pratice/pages/index.tsx
@@ -1,16 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import path from 'path';
+import fs from 'fs/promises';
 
-const inter = Inter({ subsets: ['latin'] })
+type ProductType = { 
+  id: string,
+  title: string 
+}
 
-export default function Home() {
+interface Props {
+  products: ProductType[]
+}
+
+const HomePage = (props : Props) => {
+  const { products } = props;
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   )
 }
+/** 서버사이드에서 실행되며, 브라우저 관리도구에서 코드를 확인 할 수 없음 */
+export async function getStaticProps() {
+  // cwd : 최상단 디렉토리 기준
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(`${jsonData}`);
+  return { 
+    props: {
+      products: data.products
+    }
+  };
+}
+
+export default HomePage;
