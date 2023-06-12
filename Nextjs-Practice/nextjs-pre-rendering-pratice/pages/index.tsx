6a56cb12e91bed7048dd2f8159823b332b149c7c
jeongin-devv <jeongin.devv@gmail.com>
2023-06-12 20:37:31 +0900

    소스 정리

--- Nextjs-Practice/nextjs-pre-rendering-pratice/pages/index.tsx
+++ Nextjs-Practice/nextjs-pre-rendering-pratice/pages/index.tsx
@@ -21,15 +21,21 @@ const HomePage = (props : Props) => {
   )
 }
 /** 서버사이드에서 실행되며, 브라우저 관리도구에서 코드를 확인 할 수 없음 */
+/** 빌드되는 시점에서 코드가 실행 됌 */
 export async function getStaticProps() {
-  // cwd : 최상단 디렉토리 기준
+  /**  revalidate 마다 실행 될 로깅 */
+  console.log('RE-Generating');
+  /** cwd : 최상단 디렉토리 기준 */
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(`${jsonData}`);
   return { 
     props: {
       products: data.products
-    }
+    },
+    /** 지정된 초 단위로 페이지를 재생성한다 */
+    /** 빌드 시 ISR에 확인 가능 */
+    revalidate: 10
   };
 }
 
